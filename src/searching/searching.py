7c787f9f5284c244ca64e48c65dc8e9dddb0fc8d
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-07-21 21:23:25 -0400

    searching done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if start <= end:
+        x = (start + end)// 2
+        print(start,target,end,x)
+        if arr[x] == target:
+            return x
+        elif target < arr[x]:
+            return binary_search(arr, target, start, x - 1)
+        else:
+            return binary_search(arr, target, x + 1, end)
+    else:
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +20,9 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+#def agnostic_binary_search(arr, target):
     # Your code here
 
+arr = [ 2, 3, 4, 10, 40 ] 
+target = 10 
+binary_search(arr,target,0,len(arr))
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -18,11 +18,11 @@ def merge_sort(arr):
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# # or data structures; it can only re-use the memory it was given as input
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
