ed44bf9597f581bce34929e11009389eb043bd24
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-07-22 16:29:35 -0400

    added notes

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,7 +3,7 @@ def binary_search(arr, target, start, end):
     # Your code here
     if start <= end:
         x = (start + end)// 2
-        print(start,target,end,x)
+        #print(start,target,end,x)
         if arr[x] == target:
             return x
         elif target < arr[x]:
@@ -12,6 +12,25 @@ def binary_search(arr, target, start, end):
             return binary_search(arr, target, x + 1, end)
     else:
         return -1
+#bug test
+arr = [ 2, 3, 4, 10, 40 ] 
+target = 10 
+binary_search(arr,target,0,len(arr))
+
+
+#if the start is less then the end 
+#then add the start and end together and divied by 2 that is x
+# so the array postion at x is the same as the target then return x
+# else if the target is greater then the point in the array (x) return the whole fun. replaces the end with x - 1
+#else return the fun replaces the start with x + 1
+#else return -1
+
+
+
+
+
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -23,6 +42,3 @@ def binary_search(arr, target, start, end):
 #def agnostic_binary_search(arr, target):
     # Your code here
 
-arr = [ 2, 3, 4, 10, 40 ] 
-target = 10 
-binary_search(arr,target,0,len(arr))
